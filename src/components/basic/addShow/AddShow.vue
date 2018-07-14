121761e8510008531a313b9489aac96067ffd1a3
Cristi Ciobanu <c-ciobanu@protonmail.com>
2018-07-14 12:11:47 +0200

    added Form component, Fixes #6

--- src/components/basic/addShow/AddShow.vue
+++ src/components/basic/addShow/AddShow.vue
@@ -1,31 +1,28 @@
 <template>
-  <div class="add-show">
-    <form @submit="add">
-      <Input label="Title" name="title" :blur="inputBlurHandler" />
-      <Input label="Status" name="status" :blur="inputBlurHandler" />
-      <Input label="Last Aired" name="last_aired" :blur="inputBlurHandler" />
-      <Input label="Last Seen" name="last_seen" :blur="inputBlurHandler" />
-      <Input label="Poster" name="poster" :blur="inputBlurHandler" />
-      <Button type="submit" name="add-show" text="add" importance="primary" />
-    </form>
-  </div>
+<Form :submit="addShow" submitButtonText="add">
+  <Input label="Title" name="title" :blur="inputBlurHandler" />
+  <Input label="Status" name="status" :blur="inputBlurHandler" />
+  <Input label="Last Aired" name="last_aired" :blur="inputBlurHandler" />
+  <Input label="Last Seen" name="last_seen" :blur="inputBlurHandler" />
+  <Input label="Poster" name="poster" :blur="inputBlurHandler" />
+</Form>
 </template>
 
 <script>
 import {mapActions} from "vuex";
-import Button from "@/components/utils/button/Button";
+import Form from "@/components/utils/form/Form";
 import Input from "@/components/utils/input/Input";
 
 export default {
+  components: {
+    Form,
+    Input
+  },
   props: {
     sumbitAction: Function
   },
-  components: {
-    Button,
-    Input
-  },
   methods: {
-    add: function(e) {
+    addShow: function(e) {
       e.preventDefault();
 
       let show = {
@@ -35,6 +32,7 @@ export default {
         last_seen: this._data.last_seen,
         poster: this._data.poster
       };
+
       this.$store.dispatch("shows/addShow", show);
 
       if (this.sumbitAction) {
---
+++ src/components/utils/form/Form.vue
@@ -0,0 +1,26 @@
+<template>
+  <form @submit="submit">
+    <slot></slot>
+    <Button type="submit" name="form-submit-button" :text="submitButtonText" importance="primary" />
+  </form>
+</template>
+
+<script>
+import Button from "@/components/utils/button/Button";
+
+export default {
+  components: {
+    Button
+  },
+  props: {
+    submit: Function,
+    submitButtonText: {
+      type: String,
+      default: "submit"
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
