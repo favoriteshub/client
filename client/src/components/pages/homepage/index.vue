61f835d4aba57114eaf5c7511d4873aacc40e448
Cristi Ciobanu <c-ciobanu@protonmail.com>
2018-10-08 20:45:24 +0200

    [C] homepage changes

--- client/src/components/pages/homepage/index.vue
+++ client/src/components/pages/homepage/index.vue
@@ -1,71 +1,41 @@
 <template>
-	<div class="homepage">
-		<ShowList />
-		<div class="news">
-		</div>
-		<div class="add-news">
-		</div>
-		<cButton text="add new show" importance="primary" :action="openAddShowPopup" />
-	</div>
+  <div class="homepage">
+    <div class="homepage__library">
+      <Tabs :list="[`Shows`]">
+		    <ShowList />
+      </Tabs>
+    </div>
+
+    <div class="homepage__options">
+    </div>
+  </div>
 </template>
 
 <script>
+import Tabs from "@/components/utils/tabs";
 import ShowList from "@/components/pages/homepage/ShowList";
-import AddShow from "@/components/pages/homepage/AddShow";
-import cButton from "@/components/utils/button";
 
 export default {
   components: {
-    ShowList,
-    AddShow,
-    cButton
-  },
-  methods: {
-    openAddShowPopup: function() {
-      this.$store.commit("popup/open", {
-        name: AddShow,
-        props: {sumbitAction: this.closeAddShowPopup}
-      });
-    },
-    closeAddShowPopup: function() {
-      this.$store.commit("popup/close");
-    }
+    Tabs,
+    ShowList
   }
 };
 </script>
 
 <style lang="scss" scoped>
 .homepage {
-  width: 100%;
-  height: 100%;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-areas:
-    "showcase showcase news"
-    "showcase showcase add_news"
-    "showcase showcase add_show";
-  grid-column-gap: 10px;
+  @include viewport-100();
+  display: flex;
 
-  .showcase {
-    grid-area: showcase;
+  &__library {
+    width: 70%;
+    padding: 20px;
   }
 
-  .news {
-    grid-area: news;
-    width: 200px;
-    height: 100px;
-    background-color: lightgrey;
-  }
-
-  .add-news {
-    grid-area: add_news;
-    width: 200px;
-    height: 100px;
-    background-color: grey;
-  }
-
-  button {
-    grid-area: add_show;
+  &__options {
+    width: 30%;
+    padding: 20px;
   }
 }
 </style>
