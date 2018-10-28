14c1dc215fbc691fa9afe14709b025aaa79da5e9
Cristi Ciobanu <c-ciobanu@protonmail.com>
2018-10-28 14:17:53 +0200

    [C] form refactor to conditionally render different elements

--- client/src/components/utils/form/index.vue
+++ client/src/components/utils/form/index.vue
@@ -1,30 +1,48 @@
 <template>
   <form @submit="submit">
-    <cInput
-      v-for="(field, index) in fields.list"
-      :key="index"
-      :label="field.label"
-      :name="field.name"
-      :type="field.type"
-      :disabled="field.disabled"
-      :required="field.required"
-      :blur="fields.onBlur"
-    />
+    <template v-for="(field, index) in fields">
+      <cInput
+        v-if="field.type === `input`"
+        :key="index"
+        :label="field.label"
+        :name="field.name"
+        :type="field.inputType"
+        :disabled="field.disabled"
+        :required="field.required"
+        :blur="(e) => callBack(field.name, e.target.value)"
+      />
+
+      <cSelect
+        v-if="field.type === `select`"
+        :key="index"
+        :label="field.label"
+        :name="field.name"
+        :options="field.options"
+        :onItemClick="(val) => callBack(field.name, val)"
+      />
+    </template>
+
     <cButton type="submit" name="form-submit-button" :text="submitButtonText" importance="primary" />
   </form>
 </template>
 
 <script>
 import cInput from "@/components/utils/input";
+import cSelect from "@/components/utils/select";
 import cButton from "@/components/utils/button";
 
 export default {
   components: {
     cButton,
+    cSelect,
     cInput
   },
   props: {
     submit: {
+      type: Function,
+      required: true
+    },
+    callBack: {
       type: Function,
       required: true
     },
@@ -33,7 +51,7 @@ export default {
       default: "submit"
     },
     fields: {
-      type: Object,
+      type: Array,
       required: true
     }
   }
